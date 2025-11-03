ad4de1ec5600a712a074d68d82eb4d6f565505de
“grace <gb669@cam.ac.uk>
2025-11-03 10:48:46 +0000

    remove check for elec

--- src/RetrofitModel2D.py
+++ src/RetrofitModel2D.py
@@ -1365,7 +1365,7 @@ class RetrofitModel2D:
             if c_df[col].isna().all():
                 
                 # [!!! NEW VALIDATION LOGIC !!!]
-                if "cost_per_" in col or 'elec' in col:
+                if "cost_per_" in col:
                     logger.warning(
                         f'Final cost-per-unit column "{col}" is all NaN. This is likely '
                         f'because the denominator (energy savings) is 0 for all rows. '
@@ -1377,7 +1377,10 @@ class RetrofitModel2D:
                     raise ValueError(f'Final cost column "{col}" contains all NaN values')
         
         for col in e_df.columns:
-            if e_df[col].isna().all():
+            if 'elec' in col:
+                logger.error(f'Final energy column "{col}" is all NaN for scenario {scenario}. potentially expeced for electricity')
+                
+            elif e_df[col].isna().all():
                 logger.error(f'Final energy column "{col}" is all NaN for scenario {scenario}!')
                 raise ValueError(f'Final energy column "{col}" contains all NaN values')
             
