492209c44bd248dca89c58e50b55511159d3f148
“grace <gb669@cam.ac.uk>
2025-11-26 22:31:56 +0000

    add downwcale

--- src/RetrofitDownscale.py
+++ src/RetrofitDownscale.py
@@ -2,5 +2,9 @@ import pandas as pd
 
 
 def load_scaled_gas_elec(): 
-    df = pd.read_csv('/Users/gracecolverd/RetrofitModel/notebook/joint_energ_table.csv')
+    try: 
+        df = pd.read_csv('/Users/gracecolverd/RetrofitModel/notebook/joint_energ_table.csv')
+    except:
+        df = pd.read_csv('/home/gb669/rds/hpc-work/energy_map/RetrofitModel/scaled_energy_data/joint_energ_table.csv')
+    
     return df
