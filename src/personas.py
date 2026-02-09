b8206909516bfb368326baf3927b90cdbc4ff154
“grace <gb669@cam.ac.uk>
2026-02-09 11:13:17 +0000

    update personas iwth new clusters and update epc algo plots

--- src/personas.py
+++ src/personas.py
@@ -1,27 +1,50 @@
 import pandas as pd 
 
 
-vuln_clusters = [0, 6]
-middle_clustrs = [4, 7, 8]
-upper_cl = [1, 2, 3, 5] 
+high_risk = [0, 2,3 ]
+med_risk = [10] 
+middle_risk = [5 ,8]
+low_risk = [7,9]
+v_low_risk = [1, 4, 6, 11] 
 
 mapping = {
-    **{c: 'high_deprived' for c in vuln_clusters},
-    **{c: 'med_deprived' for c in middle_clustrs},
-    **{c: 'low_deprived' for c in upper_cl}
+    **{c: 'high_risk' for c in high_risk},
+    **{c: 'med_risk' for c in med_risk},
+    **{c: 'middle_risk' for c in middle_risk},
+    **{c: 'low_risk' for c in low_risk},
+    **{c: 'v_low_risk' for c in v_low_risk}
 }
 
-name_mapping = { 
-    0: "Struggling Lone Parents",
-    1: "Secure Established Families",
-    2: "Solvent Working Households",
-    3: "Modest Solo Dwellers",
-    4: "At-Risk Singles",
-    5: "Senior Citizens",
-    6: "Isolated and Deprived",
-    7: "Younger Strugglers",
-    8: "The Squeezed Middle"
+cluster_names = {
+    0: "Diverse Urban Deprivation",
+    1: "Affluent Suburbia",
+    2: "Vulnerable Working",
+    3: "Vulnerable Elderly",
+    4: "Affluent Working Families",
+    5: "Middle England Elderly",
+    6: "Affluent Retirees",
+    7: "Thriving Diverse Working Families",
+    8: "Middle England Families",
+    9: "Off-Grid Thriving",
+    10: "Struggling urbanites",
+    11: "Highly Affluent Suburbia"
 }
+
+cluster_risks = {
+    0: "High Risk",
+    1: "Very Low Risk",
+    2: "High Risk",
+    3: "High Risk",
+    4: "Very Low Risk",
+    5: "Middle Risk",
+    6: "Very Low Risk",
+    7: "Low Risk",
+    8: "Middle Risk",
+    9: "Low Risk",
+    10: "Medium Risk",
+    11: "Very Low Risk"
+}
+
 
 # 2. Apply the mapping to your DataFrame
 # Assuming your DataFrame is named 'df' and the column is 'Cluster'
@@ -31,14 +54,12 @@ def load_personas():
     """Load persona/demographic data."""
 
     try:
-        path = '/home/gb669/rds/hpc-work/energy_map/uk_postcode_clustering/3_single_runs/regional/NE/fuel_poverty_clustering_vars13_kn9/filt_domestic_samples/clusters_res_df.csv'
+        path = '/Users/gracecolverd/Downloads/final_results_clustering_for_paoper/fuel_poc_model14_kn12/clusters_res_df.csv'
         
         personas = pd.read_csv(path)
     except:
-        path = '/Volumes/T9/2025_10_RetrofitModel/3-personas/clusters_res_df (1).csv'    
-        path ='/Users/gracecolverd/RetrofitModel/clusters_res_df (2).csv'
-        personas = pd.read_csv(path)
+       print('error cant find personas')
     print('Personas loaded')
     personas['meta_socio_persona'] = personas['cluster'].map(mapping)
-    personas['persona_name'] = personas['cluster'].map(name_mapping)
+    personas['persona_name'] = personas['cluster'].map(cluster_names)
     return personas 
